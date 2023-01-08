6ef96b490767d581e263c5578e2e2dbc89acff36
ryuji-1to <ryuji0222ss@icloud.com>
2023-01-08 21:04:52 +0900

    feat: 🎸 client load

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,3 +1,17 @@
-export const Hello = () => {
-  return <div>hello</div>;
+import { useEffect, useState } from "react";
+
+export const Hello = ({ text = "hello" }) => {
+  const [state, setState] = useState(0);
+  useEffect(() => {
+    console.log("render");
+  }, []);
+
+  return (
+    <div>
+      <p>
+        {text} {state}
+      </p>
+      <button onClick={() => setState((prev) => prev + 1)}>+1</button>
+    </div>
+  );
 };
